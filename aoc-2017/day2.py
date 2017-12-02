0e25c73be693107b5c725785d36a3481464d2802
Dan Stine <sw@stinemail.com>
2017-12-02 20:25:07 +0000

    day2

---
+++ aoc-2017/day2.py
@@ -0,0 +1,30 @@
+import sys
+
+def day2_1(spreadsheet):
+    sum = 0
+    for row in spreadsheet:
+        row_min = sys.maxint
+        row_max = 0
+        for cell in row:
+            if cell < row_min:
+                row_min = cell
+            if cell > row_max:
+                row_max = cell
+        sum += row_max - row_min
+    return sum
+
+def day2_2(spreadsheet):
+    sum = 0
+    for row in spreadsheet:
+        sum += calc_row(row)
+    return sum
+
+def calc_row(row):
+    for i in range(0, len(row)):
+        for j in range(i, len(row)):
+            if i != j:
+                max_val = max(row[i], row[j])
+                min_val = min(row[i], row[j])
+                if max_val % min_val == 0:
+                    return max_val // min_val
+    raise Exception("uh, oh!")
---
+++ aoc-2017/day2_test.py
@@ -0,0 +1,22 @@
+import csv
+import day2
+import unittest
+
+class Day2Test(unittest.TestCase):
+    def test1(self):
+        spreadsheet = read_file("aoc/aoc-2017/day2_1_example.csv")
+        self.assertEqual(day2.day2_1(spreadsheet), 18)
+    def test2(self):
+        spreadsheet = read_file("aoc/aoc-2017/day2_2_example.csv")
+        self.assertEqual(day2.day2_2(spreadsheet), 9)
+
+def read_file(filename):
+    contents = []
+    with open(filename) as file:
+        reader = csv.reader(file, delimiter='\t')
+        for line in reader:
+            contents.append(map(int, line))
+    return contents
+
+if __name__ == '__main__':
+    unittest.main()
