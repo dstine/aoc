7ee60918c2dae78b886fb8409bdff4d5bcf960b9
Dan Stine <sw@stinemail.com>
2017-12-03 01:16:29 +0000

    introduce data directory

--- aoc-2017/day2_test.py
+++ aoc-2017/day2_test.py
@@ -4,10 +4,10 @@ import unittest
 
 class Day2Test(unittest.TestCase):
     def test1(self):
-        spreadsheet = read_file("aoc/aoc-2017/day2_1_example.csv")
+        spreadsheet = read_file("aoc/aoc-2017/data/day2_1_example.csv")
         self.assertEqual(day2.day2_1(spreadsheet), 18)
     def test2(self):
-        spreadsheet = read_file("aoc/aoc-2017/day2_2_example.csv")
+        spreadsheet = read_file("aoc/aoc-2017/data/day2_2_example.csv")
         self.assertEqual(day2.day2_2(spreadsheet), 9)
 
 def read_file(filename):
