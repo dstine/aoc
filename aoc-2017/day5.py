2c958a18ecc670ff014c0c3a3ffc1f4b09ec1b68
Dan Stine <sw@stinemail.com>
2017-12-06 01:28:22 +0000

    day5 part 2

--- aoc-2017/day5.py
+++ aoc-2017/day5.py
@@ -1,4 +1,16 @@
 def day5_1(instructions):
+    return day5(instructions, offset_by_one)
+
+def day5_2(instructions):
+    return day5(instructions, offset_strange)
+
+def offset_by_one(offset):
+    return 1
+
+def offset_strange(offset):
+    return -1 if offset >= 3 else 1
+
+def day5(instructions, offset_rule):
     ptr = 0
     jumps = 0
     while True:
@@ -7,7 +19,7 @@ def day5_1(instructions):
         jumps += 1
         #print("{} {} {} {} {}".format(instructions, ptr, jumps, offset, new_ptr))
         if 0 <= new_ptr < len(instructions):
-            instructions[ptr] += 1
+            instructions[ptr] += offset_rule(offset)
             ptr = new_ptr
         else:
             return jumps
--- aoc-2017/day5_test.py
+++ aoc-2017/day5_test.py
@@ -8,6 +8,10 @@ class Day5Test(unittest.TestCase):
         self.assertEqual(day5.day5_1([0, 3,  0,  1,  -3]), 5)
         self.assertEqual(day5.day5_1(MY_INPUT), 372671)
 
+    def test_day5_1(self):
+        self.assertEqual(day5.day5_2([0, 3,  0,  1,  -3]), 10)
+        self.assertEqual(day5.day5_2(MY_INPUT), 25608480)
+
 def read_file(filename):
     with open(filename) as file:
         content = file.readlines()
