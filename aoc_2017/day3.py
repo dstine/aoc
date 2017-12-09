cb5fa1f1caea187c5e4a18657d45e9533ce3257e
Dan Stine <sw@stinemail.com>
2017-12-09 23:33:57 +0000

    improve day3 part 1

--- aoc_2017/day3.py
+++ aoc_2017/day3.py
@@ -1,27 +1,45 @@
-import math
-
+# observe that for each "ring" of the grid, we take the same number of steps
+# for each of the four directions
+#   allocate  9 -  1 =  8 = 2 in each of 4 directions
+#   allocate 25 -  9 = 16 = 4 in each of 4 directions
+#   allocate 49 - 25 = 24 = 6 in each of 4 directions
 def day3_1(num):
-    if num == 1:
-        return 0
-        
-    ring_edge_len = ring_edge(num)
-    modulus = ring_edge_len - 1
-    vals = []
-    increment = 1
-    val = modulus - 1
-    for _ in range(0, modulus):
-        vals.append(val)
-        val += increment
-        if val == modulus:
-            increment = -1
-        if val == modulus // 2:
-            increment = 1
+    location = [0,0]
+    value = 1
+    grid = {}
 
-    idx = num % modulus
-    return vals[idx]
+    update(grid, value, location)
+    value += 1
 
-def ring_edge(num):
-    ring_edge_len = int(math.ceil(math.sqrt(num)))
-    if ring_edge_len % 2 == 0:
-        ring_edge_len += 1
-    return ring_edge_len
+    steps_per_dir = 0
+    while value <= num:
+        steps_per_dir += 2
+        step_right(location)
+        step_down(location)
+        value = fill_edge(grid, value, location, steps_per_dir, step_up)
+        value = fill_edge(grid, value, location, steps_per_dir, step_left)
+        value = fill_edge(grid, value, location, steps_per_dir, step_down)
+        value = fill_edge(grid, value, location, steps_per_dir, step_right)
+    return abs(grid[num][0]) + abs(grid[num][1])
+
+def fill_edge(grid, value, location, steps_per_dir, take_step):
+    for i in range(steps_per_dir):
+        take_step(location)
+        update(grid, value, location)
+        value += 1
+    return value
+
+def step_right(location):
+    location[0] += 1
+
+def step_up(location):
+    location[1] += 1
+
+def step_left(location):
+    location[0] -= 1
+
+def step_down(location):
+    location[1] -= 1
+
+def update(grid, value, location):
+    grid[value] = tuple(location)
