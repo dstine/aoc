9ad857772d52bff0dce63740b64a7b75310aa7dc
Dan Stine <sw@stinemail.com>
2017-12-11 00:57:22 +0000

    day10 part 2

--- aoc_2017/day10.py
+++ aoc_2017/day10.py
@@ -1,15 +1,22 @@
 def day10_1(lst_length, lengths_input):
+    return day10(lst_length, lengths_input, calc_lengths_1, 1, product_first_two)
+
+def day10_2(lst_length, lengths_input):
+    return day10(lst_length, lengths_input, calc_lengths_2, 64, hash_in_hex)
+
+def day10(lst_length, lengths_input, calc_lengths, rounds, calc_result):
     lst = [n for n in range(0, lst_length)]
-    lengths = [int(n) for n in lengths_input.split(',')]
+    lengths = calc_lengths(lengths_input)
     skip_size = 0
     curr_pos = 0
-    for length in lengths:
-        indexes = calc_indexes(curr_pos, lst, length)
-        reverse(indexes, lst)
-        curr_pos += length + skip_size
-        curr_pos %= len(lst)
-        skip_size += 1
-    return lst[0] * lst[1]
+    for _ in range(rounds):
+        for length in lengths:
+            indexes = calc_indexes(curr_pos, lst, length)
+            reverse(indexes, lst)
+            curr_pos += length + skip_size
+            curr_pos %= len(lst)
+            skip_size += 1
+    return calc_result(lst)
 
 def calc_indexes(curr_pos, lst, length):
     indexes = list(range(curr_pos, curr_pos + length))
@@ -24,3 +31,37 @@ def reverse(indexes, lst):
         tmp = lst[first_idx]
         lst[first_idx] = lst[last_idx]
         lst[last_idx] = tmp
+
+def product_first_two(lst):
+    return lst[0] * lst[1]
+
+BLOCK_SIZE = 16
+
+def hash_in_hex(lst):
+    dense_hash = []
+    for i in range(BLOCK_SIZE):
+        dense_hash.append(bitwise_xor(lst[BLOCK_SIZE*i:BLOCK_SIZE*i + BLOCK_SIZE]))
+    hex_hash = [hex(n).replace('0x', '') for n in dense_hash]
+    hex_hash = [h if len(h) == 2 else '0'+h for h in hex_hash]
+    ans = ''
+    for i in range(BLOCK_SIZE):
+        ans += hex_hash[i]
+    return ans
+
+def bitwise_xor(sub_lst):
+    acc = 0
+    for i in range(0, len(sub_lst)):
+        acc ^= sub_lst[i]
+    return acc
+
+def calc_lengths_1(lengths_input):
+    return [int(n) for n in lengths_input.split(',')]
+
+STANDARD_LENGTH_SUFFIX = [17, 31, 73, 47, 23]
+
+def calc_lengths_2(lengths_input):
+    lengths = as_ascii(lengths_input)
+    return lengths + STANDARD_LENGTH_SUFFIX
+
+def as_ascii(lengths_input):
+    return [ord(char) for char in lengths_input]
--- aoc_2017/day10_test.py
+++ aoc_2017/day10_test.py
@@ -2,9 +2,20 @@ import day10
 import unittest
 
 class Day10Test(unittest.TestCase):
-    def test_day9_1(self):
-        self.assertEqual(day10.day10_1(5, '3, 4, 1, 5'), 12)
+    def test_day10_1(self):
+        self.assertEqual(day10.day10_1(5, '3,4,1,5'), 12)
         self.assertEqual(day10.day10_1(256, MY_INPUT), 6909)
+    def test_as_ascii(self):
+        self.assertEqual(day10.as_ascii('3,4,1,5'), [51, 44, 52, 44, 49, 44, 53])
+        self.assertEqual(day10.as_ascii('1,2,3'), [49,44,50,44,51])
+    def test_bitwise_xor(self):
+        self.assertEqual(day10.bitwise_xor([65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22]), 64)
+    def test_day10_2(self):
+        self.assertEqual(day10.day10_2(256, ''), 'a2582a3a0e66e6e86e3812dcb672a272')
+        self.assertEqual(day10.day10_2(256, 'AoC 2017'), '33efeb34ea91902bb2f59c9920caa6cd')
+        self.assertEqual(day10.day10_2(256, '1,2,3'), '3efbe78a8d82f29979031a4aa0b16a9d')
+        self.assertEqual(day10.day10_2(256, '1,2,4'), '63960835bcdc130f0b66d7ff4f6a5a8e')
+        self.assertEqual(day10.day10_2(256, MY_INPUT), '9d5f4561367d379cfbf04f8c471c0095')
 
 def readfile(filename):
     with open(filename) as file:
