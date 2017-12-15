55bec2ec8ac3fa3bb999b0bdf5ef223c2bf5b0e3
Dan Stine <sw@stinemail.com>
2017-12-15 16:20:17 +0000

    day12 part 2

--- aoc_2017/day12.py
+++ aoc_2017/day12.py
@@ -1,21 +1,31 @@
 import re
 
 def day12_1(filename):
-    lines = readfile(filename)
-    dict = parse_lines(lines)
-    programs_to_process = [0]
-    return process_group(dict, programs_to_process)
+    return day12(filename)[0]
 
-def process_group(dict, programs_to_process):
+def day12_2(filename):
+    return day12(filename)[1]
+
+def day12(filename):
+    lines = readfile(filename)
+    programs_dict = parse_lines(lines)
+    groups_dict = {}
+    while len(programs_dict) > 0:
+        seed_program = list(programs_dict.keys())[0]
+        groups_dict[seed_program] = find_programs_in_group(programs_dict, seed_program)
+    return len(groups_dict[0]), len(groups_dict)
+
+def find_programs_in_group(programs_dict, seed_program):
+    programs_to_process = [seed_program]
     programs_in_group = set()
     while len(programs_to_process) > 0:
         program = programs_to_process.pop(0)
         programs_in_group.add(program)
-        linked_programs = dict.pop(program, None)
+        linked_programs = programs_dict.pop(program, None)
         if linked_programs:
             for linked_program in linked_programs:
                 programs_to_process.append(linked_program)
-    return len(programs_in_group)
+    return programs_in_group
 
 def readfile(filename):
     with open(filename) as file:
@@ -23,11 +33,11 @@ def readfile(filename):
         return [line.rstrip() for line in lines]
 
 def parse_lines(lines):
-    dict = {}
+    programs_dict = {}
     for line in lines:
         parsed = parse_line(line)
-        dict[int(parsed[0])] = [int(n) for n in parsed[1]]
-    return dict
+        programs_dict[int(parsed[0])] = [int(n) for n in parsed[1]]
+    return programs_dict
 
 def parse_line(line):
     program_and_linked_programs = re.search('([0-9]+) <-> ([0-9, ]+)', line)
--- aoc_2017/day12_test.py
+++ aoc_2017/day12_test.py
@@ -5,6 +5,9 @@ class Day12Test(unittest.TestCase):
     def test_day12_1(self):
         self.assertEqual(day12.day12_1('aoc/aoc_2017/data/day12_example.txt'), 6)
         self.assertEqual(day12.day12_1(MY_INPUT), 288)
+    def test_day12_2(self):
+        self.assertEqual(day12.day12_2('aoc/aoc_2017/data/day12_example.txt'), 2)
+        self.assertEqual(day12.day12_2(MY_INPUT), 211)
 
 MY_INPUT = 'aoc/aoc_2017/data/day12_real.txt'
 
