cd4eb037e33c0c620ac13104924e2fb53702c649
Dan Stine <sw@stinemail.com>
2017-12-19 00:42:19 +0000

    day15 part 2

--- aoc_2017/day15.py
+++ aoc_2017/day15.py
@@ -1,19 +1,33 @@
+def day15_1(a_start, b_start, num_pairs):
+    return day15(a_start, 1, b_start, 1, num_pairs)
+
+def day15_2(a_start, b_start, num_pairs):
+    return day15(a_start, 4, b_start, 8, num_pairs)
+
 A_FACTOR = 16807
 B_FACTOR = 48271
 DIVISOR = 2147483647
-MASK = 2 ** 16
 
-def day15_1(a_start, b_start, num_pairs):
+def day15(a_start, a_multiple_of, b_start, b_multiple_of, num_pairs):
     iterations = 0
     matches = 0
     a = a_start
     b = b_start
     while iterations < num_pairs:
-        a = a * A_FACTOR % DIVISOR
-        b = b * B_FACTOR % DIVISOR
-        a_lo16 = bin(a % MASK)
-        b_lo16 = bin(b % MASK)
+        a = generate_next(a, A_FACTOR, a_multiple_of)
+        b = generate_next(b, B_FACTOR, b_multiple_of)
+        a_lo16 = a & 0xffff
+        b_lo16 = b & 0xffff
+        a_lo16 = bin(a_lo16)
+        b_lo16 = bin(b_lo16)
         if a_lo16 == b_lo16:
             matches += 1
         iterations += 1
     return matches
+
+def generate_next(current, factor, multiple_of):
+    candidate = current
+    while True:
+        candidate = candidate * factor % DIVISOR
+        if candidate % multiple_of == 0:
+            return candidate
--- aoc_2017/day15_test.py
+++ aoc_2017/day15_test.py
@@ -3,10 +3,13 @@ import unittest
 
 class Day15Test(unittest.TestCase):
     def test_day15_1(self):
-        self.assertEqual(day15.day15_1(65, 8921, NUM_PAIRS), 588)
-        self.assertEqual(day15.day15_1(516, 190, NUM_PAIRS), 597)
-
-NUM_PAIRS = 40000000
+        num_pairs = 40000000
+        self.assertEqual(day15.day15_1(65, 8921, num_pairs), 588)
+        self.assertEqual(day15.day15_1(516, 190, num_pairs), 597)
+    def test_day15_2(self):
+        num_pairs = 5000000
+        self.assertEqual(day15.day15_2(65, 8921, num_pairs), 309)
+        self.assertEqual(day15.day15_2(516, 190, num_pairs), 303)
 
 if __name__ == '__main__':
     unittest.main()
