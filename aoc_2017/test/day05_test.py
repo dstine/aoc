49b2823ea300074e2728fb27c02354675c850928
Dan Stine <sw@stinemail.com>
2017-12-25 01:10:18 +0000

    fix unit test

--- aoc_2017/test/day05_test.py
+++ aoc_2017/test/day05_test.py
@@ -7,11 +7,11 @@ class Day05Test(unittest.TestCase):
 
     def test_day5_1(self):
         self.assertEqual(day05.day5_1([0, 3,  0,  1,  -3]), 5)
-        self.assertEqual(day05.day5_1(MY_INPUT), 372671)
+        self.assertEqual(day05.day5_1(list(MY_INPUT)), 372671)
 
     def test_day5_2(self):
         self.assertEqual(day05.day5_2([0, 3,  0,  1,  -3]), 10)
-        self.assertEqual(day05.day5_2(MY_INPUT), 25608480)
+        self.assertEqual(day05.day5_2(list(MY_INPUT)), 25608480)
 
 def read_file(filename):
     with open(filename) as file:
