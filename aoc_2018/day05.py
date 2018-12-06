6d9bd5324e0f18d8962c0a7cbb8ab85edf16b80c
Dan Stine <sw@stinemail.com>
2018-12-06 04:00:46 +0000

    2018 day 5 improve run time

--- aoc_2018/day05.py
+++ aoc_2018/day05.py
@@ -16,12 +16,17 @@ def part2(polymer):
 
 # recursive approach blew the stack
 def react(polymer):
+    # pre-compute information about each character for performance
+    # for my puzzle input, reduced runtime by about 60%
+    polymer = [(ch, ch.lower(), ch.islower()) for ch in polymer]
     prior = ''
     reacted = polymer
     while prior != reacted:
         prior = reacted
         reacted = react_once(prior)
-    return reacted
+    # extract just the string to return the result
+    reacted_polymer = ''.join([c[0] for c in reacted])
+    return reacted_polymer
 
 def react_once(polymer):
     out = []
@@ -31,16 +36,34 @@ def react_once(polymer):
     while i < last_index:
         c1 = polymer[i]
         c2 = polymer[i+1]
-        same_letter = c1.lower() == c2.lower()
-        different_case = c1.islower() != c2.islower()
+        same_letter = c1[1] == c2[1]    # lower()
+        different_case = c1[2] != c2[2] # islower()
         if same_letter and different_case:
             # reaction
             i += 2
         else:
             # no reaction
-            out += c1
+            out.append(polymer[i])
             i += 1
         if i == last_index:
-            out += polymer[i]
+            out.append(polymer[i])
 
-    return ''.join(out)
+    return out
+
+# used for profiling:
+#     python3 -m cProfile day05.py
+if __name__ == '__main__':
+
+    # duplicated from test file
+    def get_my_input():
+        path = 'data/day05_input.txt'
+        return get_one_liner(path)
+
+    def get_one_liner(path):
+        with open(path) as file:
+            # only one line in file
+            for line in file:
+                return line.rstrip()
+
+    part2('dabAcCaCBAcCcaDA')
+    part2(get_my_input())
