22d2d70282603a0c036a7134fd329609fa3d9ba3
Dan Stine <sw@stinemail.com>
2018-12-07 02:17:00 +0000

    2018 day 6 part 1

---
+++ aoc_2018/day06.py
@@ -0,0 +1,48 @@
+import sys
+from collections import defaultdict
+
+def part1(coords):
+    x_lo = min([coord[0] for coord in coords])
+    x_hi = max([coord[0] for coord in coords])
+    y_lo = min([coord[1] for coord in coords])
+    y_hi = max([coord[1] for coord in coords])
+    grid = defaultdict(lambda: -1)
+    for x in range(x_lo, x_hi + 1):
+        for y in range(y_lo, y_hi + 1):
+            grid[(x, y)] = label_cell((x, y), coords)
+
+    edge_labels = set()
+    for x in range(x_lo, x_hi + 1):
+        edge_labels.add(grid[(x, y_lo)])
+        edge_labels.add(grid[(x, y_hi)])
+    for y in range(y_lo, y_hi + 1):
+        edge_labels.add(grid[(x_lo, y)])
+        edge_labels.add(grid[(x_hi, y)])
+
+    all_labels = set(range(len(coords)))
+    interior_labels = all_labels - edge_labels
+    counts = defaultdict(int)
+    for x in range(x_lo, x_hi + 1):
+        for y in range(y_lo, y_hi + 1):
+            label = grid[(x, y)]
+            if label in interior_labels:
+                counts[label] += 1
+    return max(counts.values())
+
+def label_cell(cell, coords):
+    closest_distance = sys.maxsize
+    closest_indexes = [-1]
+    for i, coord in enumerate(coords):
+        dist = distance(cell, coord)
+        if dist < closest_distance:
+            closest_distance = dist
+            closest_index = [i]
+        elif dist == closest_distance:
+            closest_index.append(i)
+    if len(closest_index) > 1:
+        return -1
+    else:
+        return closest_index[0]
+
+def distance(c1, c2):
+    return abs(c1[0] - c2[0]) + abs(c1[1] - c2[1])
---
+++ aoc_2018/test/day06_test.py
@@ -0,0 +1,34 @@
+import aoc_2018.test.test_utils as test_utils
+import aoc_2018.day06 as day06
+import pytest
+
+example_coords = [
+    (1, 1), 
+    (1, 6), 
+    (8, 3), 
+    (3, 4), 
+    (5, 5), 
+    (8, 9),
+]
+
+def test_part1():
+    assert day06.part1(example_coords) == 17
+    assert day06.part1(get_my_input()) == 3647
+
+def test_label_cell():
+    assert day06.label_cell((0, 0), example_coords) == 0
+    assert day06.label_cell((3, 4), example_coords) == 3
+    assert day06.label_cell((2, 5), example_coords) == -1
+
+def test_distance():
+    assert day06.distance((0, 0), (1, 1)) == 2
+    assert day06.distance((6, 3), (1, 1)) == 7
+    assert day06.distance((10, 3), (4, 5)) == 8
+
+def get_my_input():
+    path = test_utils.get_path('day06_input.txt')
+    with open(path) as file:
+        lines = [line.rstrip() for line in file]
+    coords = [(line.split(', ')) for line in lines]
+    coords = [(int(c[0]), int(c[1])) for c in coords]
+    return coords
