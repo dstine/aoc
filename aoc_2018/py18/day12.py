d124ec25104b95f7275a6e29301bbcd9cd4474fb
Dan Stine <sw@stinemail.com>
2018-12-13 03:46:50 +0000

    2018 day 12 part 1

---
+++ aoc_2018/py18/day12.py
@@ -0,0 +1,32 @@
+def part1(initial_state, rules, n_generations):
+    #print()
+    state = {}
+    for i in range(len(initial_state)):
+        state[i] = initial_state[i]
+    #print('{:2d}: {}'.format(0, state_as_string(state)))
+    for _ in range(1, n_generations+1):
+        state = generation(state, rules)
+        #print('{:2d}: {}'.format(gen, state_as_string(state)))
+    return sum([num for num, pot in state.items() if pot == '#'])
+
+def generation(state, rules):
+    next_state = state.copy()
+    for num in range(min(state.keys()), max(state.keys())+1):
+        window = []
+        for n in range(-2, 3):
+            idx = num + n
+            if idx not in state:
+                state[idx] = '.'
+                next_state[idx] = '.'
+            window.append(state[idx])
+        window = ''.join(window)
+        next_val = rules[window] if window in rules else '.'
+        #print('    {:2d} {}   {}'.format(num, pots, next_val))
+        next_state[num] = next_val
+    return next_state
+
+def state_as_string(state):
+    val = []
+    for i in range(min(state.keys()), max(state.keys())+1):
+        val.append(state[i])
+    return ''.join(val)
---
+++ aoc_2018/py18/test/day12_test.py
@@ -0,0 +1,31 @@
+import py18.test.test_utils as test_utils
+import py18.day12 as day12
+import pytest
+
+def test_part1():
+    initial_state, rules = get_example_input()
+    assert day12.part1(initial_state, rules, 20) == 325
+    initial_state, rules = get_my_input()
+    assert day12.part1(initial_state, rules, 20) == 2049
+
+def get_my_input():
+    path = test_utils.get_path('day12_input.txt')
+    return get_input(path)
+
+def get_example_input():
+    path = test_utils.get_path('day12_example.txt')
+    return get_input(path)
+
+def get_input(path):
+    initial_state = ''
+    rules = {}
+    with open(path) as file:
+        for line in file.readlines():
+            line = line.rstrip()
+            if line.startswith('initial state: '):
+                initial_state = line.split('initial state: ')[1]
+            elif line:
+                pattern, result = line.split(' => ')
+                rules[pattern] = result
+    return initial_state, rules
+
