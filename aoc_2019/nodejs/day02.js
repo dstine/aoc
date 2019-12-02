3d37b9c036a711cff62bfea24821c32320d10586
Dan Stine <sw@stinemail.com>
2019-12-02 23:26:47 +0000

    2019 day 2 part 1

---
+++ aoc_2019/nodejs/day02.js
@@ -0,0 +1,33 @@
+module.exports = {
+    run_program: run_program,
+    part1: part1,
+};
+
+function run_program(opcodes) {
+    var pc = 0;
+    while (pc < opcodes.length) {
+        var opcode = opcodes[pc];
+        switch (opcode) {
+            case 1:
+                opcodes[opcodes[pc+3]] = opcodes[opcodes[pc+1]] + opcodes[opcodes[pc+2]];
+                break;
+            case 2:
+                opcodes[opcodes[pc+3]] = opcodes[opcodes[pc+1]] * opcodes[opcodes[pc+2]];
+                break;
+            case 99:
+                return opcodes[0];
+            default:
+                throw `unknown opcode: ${opcode}`;
+        }
+        pc += 4;
+    }
+    return opcodes[0];
+}
+
+function part1(opcodes) {
+    opcodes[1] = 12;
+    opcodes[2] = 2;
+    console.log(typeof(opcodes[0]))
+    console.log(typeof(opcodes[1]))
+    return run_program(opcodes);
+}
