155e6d040b3f718ef82e7a263726aaa2a173d1fd
Dan Stine <sw@stinemail.com>
2019-12-04 03:53:22 +0000

    more test cases

--- aoc_2019/nodejs/test/day03_test.js
+++ aoc_2019/nodejs/test/day03_test.js
@@ -31,6 +31,12 @@ describe('day03', function() {
         var w1_moves = ['R8','U5','L5','D3'];
         var w2_moves = ['U7','R6','D4','L4'];
         assert.equal(day03.part1(w1_moves, w2_moves), 6);
+        w1_moves = 'R75,D30,R83,U83,L12,D49,R71,U7,L72'.split(',');
+        w2_moves = 'U62,R66,U55,R34,D71,R55,D58,R83'.split(',');
+        assert.equal(day03.part1(w1_moves, w2_moves), 159);
+        w1_moves = 'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51'.split(',');
+        w2_moves = 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'.split(',');
+        assert.equal(day03.part1(w1_moves, w2_moves), 135);
     });
     it('part1', function () {
         var moves = utils.get_input('day03');
