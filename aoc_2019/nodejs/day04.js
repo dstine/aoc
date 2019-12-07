48b6529e9bffccf64ba4fb8eb9f760c2f74c96ed
Dan Stine <sw@stinemail.com>
2019-12-07 03:42:41 +0000

    2019 day 4 part 2

--- aoc_2019/nodejs/day04.js
+++ aoc_2019/nodejs/day04.js
@@ -1,26 +1,37 @@
 module.exports = {
     is_candidate,
     part1,
+    is_candidate2,
+    part2,
 };
 
 function is_candidate(n) {
     var digits = n.toString().split('');
     var has_double = false;
-    for (var i=0; i<digits.length-1; i++) {
+    var prior_digit = digits[0];
+    var i = 1;
+    while (i < digits.length) {
+        var curr_digit = digits[i];
         // Two adjacent digits are the same (like 22 in 122345).
-        if (!has_double && (digits[i] == digits[i+1])) {
+        if (curr_digit == prior_digit) {
             has_double = true;
         }
         // Going from left to right, the digits never decrease;
         // they only ever increase or stay the same (like 111123 or 135679).
-        if (digits[i] > digits[i+1]) {
+        if (curr_digit < prior_digit) {
             return false;
         }
+        prior_digit = curr_digit;
+        i += 1;
     }
     return has_double;
 }
 
 function part1(input) {
+    return part(input, is_candidate);
+}
+
+function part(input, is_candidate) {
     var range = input.split('-').map(Number);
     var start = range[0];
     var end = range[1];
@@ -31,4 +42,43 @@ function part1(input) {
         }
     }
     return count;
+}
+
+function is_candidate2(n) {
+    var digits = n.toString().split('');
+    var double_vals = [];
+    var prior_prior_digit = digits[0];
+    var prior_digit = digits[1];
+    if (prior_digit == prior_prior_digit) {
+        double_vals.push(prior_digit);
+    }
+    if (prior_digit < prior_prior_digit) {
+        return false;
+    }
+    var i = 2;
+    while (i < digits.length) {
+        var curr_digit = digits[i];
+        // Two adjacent digits are the same (like 22 in 122345).
+        // AND are not part of a larger group of matching digits
+        if (curr_digit == prior_digit) {
+            if (prior_digit != prior_prior_digit) {
+                double_vals.push(curr_digit);
+            } else if (double_vals.includes(curr_digit)) {
+                double_vals.pop();
+            }
+        }
+        // Going from left to right, the digits never decrease;
+        // they only ever increase or stay the same (like 111123 or 135679).
+        if (curr_digit < prior_digit) {
+            return false;
+        }
+        prior_prior_digit = prior_digit;
+        prior_digit = curr_digit;
+        i += 1;
+    }
+    return double_vals.length > 0;
+}
+
+function part2(input) {
+    return part(input, is_candidate2);
 }
--- aoc_2019/nodejs/test/day04_test.js
+++ aoc_2019/nodejs/test/day04_test.js
@@ -12,4 +12,33 @@ describe('day04', function() {
     it('part1', function () {
         assert.equal(day04.part1('130254-678275'), 2090);
     });
+    it('is_candidate2', function () {
+        assert.equal(day04.is_candidate2(111111), false);
+        assert.equal(day04.is_candidate2(223450), false);
+        assert.equal(day04.is_candidate2(123455), true);
+        assert.equal(day04.is_candidate2(123789), false);
+        assert.equal(day04.is_candidate2(112233), true);
+        assert.equal(day04.is_candidate2(123444), false);
+        assert.equal(day04.is_candidate2(111122), true);
+        
+        assert.equal(day04.is_candidate2(111234), false);
+        assert.equal(day04.is_candidate2(222211), false);
+        assert.equal(day04.is_candidate2(111120), false);
+        assert.equal(day04.is_candidate2(201556), false);
+        assert.equal(day04.is_candidate2(223456), true);
+        assert.equal(day04.is_candidate2(222456), false);
+        assert.equal(day04.is_candidate2(113456), true);
+        assert.equal(day04.is_candidate2(122456), true);
+        assert.equal(day04.is_candidate2(123356), true);
+        assert.equal(day04.is_candidate2(123446), true);
+        assert.equal(day04.is_candidate2(123455), true);
+        assert.equal(day04.is_candidate2(112455), true);
+        assert.equal(day04.is_candidate2(122455), true);
+
+        // double followed by triple
+        assert.equal(day04.is_candidate2(112225), true);
+    });
+    it('part2', function () {
+        assert.equal(day04.part2('130254-678275'), 1419);
+    });
 });
