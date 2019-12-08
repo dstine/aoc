1d880bd9a7b8baf36b1b5ec8730d8aa730d0a91c
Dan Stine <sw@stinemail.com>
2019-12-08 04:20:34 +0000

    2019 day 5 part 2

--- aoc_2019/nodejs/day05.js
+++ aoc_2019/nodejs/day05.js
@@ -1,7 +1,7 @@
 module.exports = {
     run_program: run_program,
     part1: part1,
-    // part2: part2,
+    part2: part2,
 };
 
 var debug = false;
@@ -9,36 +9,57 @@ var debug = false;
 function run_program(memory, input) {
     var outputs = [];
     var iptr = 0;
-    var incr = 0;
     var halt = false;
     while (iptr < memory.length && !halt) {
         var iword = memory[iptr];
         var opcode = iword % 1e2;
         var modes = Math.trunc(iword / 1e2);
-        if (debug) console.log(`op${opcode}`);
-        if ([1,2].includes(opcode)) {
+        if (debug) {
+            console.log('--------------');
+            console.log(`memory ${memory}`);
+            console.log(`iptr   ${iptr}`);
+            console.log(`op     ${opcode}`);
+        }
+        if ([1,2,7,8].includes(opcode)) {
             var val1 = deref(memory, iptr, modes, 1);
             var val2 = deref(memory, iptr, modes, 2);
             var result_loc = decode(memory, iptr, modes, 3);
-            if (opcode == 1) {
+            if (1 == opcode) {
                 memory[result_loc] = val1 + val2;
-            } else {
+            } else if (2 == opcode) {
                 memory[result_loc] = val1 * val2;
+            } else if (7 == opcode) {
+                memory[result_loc] = val1 < val2 ? 1 : 0;
+            } else if (8 == opcode) {
+                memory[result_loc] = val1 == val2 ? 1 : 0;
             }
-            incr = 4;
+            iptr += 4;
         } else if (3 == opcode) {
             memory[decode(memory, iptr, modes, 1)] = input;
-            incr = 2;
+            iptr += 2;
         } else if (4 == opcode) {
             var output = deref(memory, iptr, modes, 1);
             outputs.push(output);
-            incr = 2;
+            iptr += 2;
+        } else if (5 == opcode) {
+            val1 = deref(memory, iptr, modes, 1);
+            if (val1) {
+                iptr = deref(memory, iptr, modes, 2);
+            } else {
+                iptr += 3;
+            }
+        } else if (6 == opcode) {
+            val1 = deref(memory, iptr, modes, 1);
+            if (val1 == 0) {
+                iptr = deref(memory, iptr, modes, 2);
+            } else {
+                iptr += 3;
+            }
         } else if (99 == opcode) {
             halt = true;
         } else {
             throw `unknown opcode: ${opcode}`;
         }
-        iptr += incr;
     }
     var diagnostic_code = memory[0];
     outputs.push(diagnostic_code);
@@ -56,4 +77,8 @@ function decode(memory, iptr, modes, offset) {
 
 function part1(memory) {
     return run_program(memory, 1);
+}
+
+function part2(memory) {
+    return run_program(memory, 5);
 }
--- aoc_2019/nodejs/test/day05_test.js
+++ aoc_2019/nodejs/test/day05_test.js
@@ -4,12 +4,51 @@ var utils = require('./test_utils.js');
 var day05 = require('../day05.js');
 
 describe('day02', function() {
-    it('run_program', function () {
+    it('run_program_ops34', function () {
         assert.deepEqual(day05.run_program([1002,4,3,4,33], 123456), [1002]);
     });
     it('part1', function () {
         var memory = utils.get_input('day05', ',', Number);
         // TODO: why is there a trailing 3?
         assert.deepEqual(day05.part1(memory), [0, 0, 0, 0, 0, 0, 0, 0, 0, 6069343, 3]);
+    });
+    it('run_program_ops78', function () {
+        // TODO: why is there a trailing 3?
+
+        assert.deepEqual(day05.run_program([3,9,8,9,10,9,4,9,99,-1,8], 7), [0, 3]);
+        assert.deepEqual(day05.run_program([3,9,8,9,10,9,4,9,99,-1,8], 8), [1, 3]);
+        assert.deepEqual(day05.run_program([3,9,8,9,10,9,4,9,99,-1,8], 9), [0, 3]);
+
+        assert.deepEqual(day05.run_program([3,9,7,9,10,9,4,9,99,-1,8], 7), [1, 3]);
+        assert.deepEqual(day05.run_program([3,9,7,9,10,9,4,9,99,-1,8], 8), [0, 3]);
+        assert.deepEqual(day05.run_program([3,9,7,9,10,9,4,9,99,-1,8], 9), [0, 3]);
+
+        assert.deepEqual(day05.run_program([3,3,1108,-1,8,3,4,3,99], 7), [0, 3]);
+        assert.deepEqual(day05.run_program([3,3,1108,-1,8,3,4,3,99], 8), [1, 3]);
+        assert.deepEqual(day05.run_program([3,3,1108,-1,8,3,4,3,99], 9), [0, 3]);
+
+        assert.deepEqual(day05.run_program([3,3,1107,-1,8,3,4,3,99], 7), [1, 3]);
+        assert.deepEqual(day05.run_program([3,3,1107,-1,8,3,4,3,99], 8), [0, 3]);
+        assert.deepEqual(day05.run_program([3,3,1107,-1,8,3,4,3,99], 9), [0, 3]);
+    });
+    it('run_program_ops56', function () {
+        // TODO: why is there a trailing 3?
+        assert.deepEqual(day05.run_program([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], 0), [0, 3]);
+        assert.deepEqual(day05.run_program([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], 1), [1, 3]);
+        assert.deepEqual(day05.run_program([3,3,1105,-1,9,1101,0,0,12,4,12,99,1], 0), [0, 3]);
+        assert.deepEqual(day05.run_program([3,3,1105,-1,9,1101,0,0,12,4,12,99,1], 1), [1, 3]);
+
+    });
+    it('run_program_larger', function () {
+        // TODO: why is there a trailing 3?
+        var memory = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99];
+        assert.deepEqual(day05.run_program(memory, 7), [999, 3]);
+        assert.deepEqual(day05.run_program(memory, 8), [1000, 3]);
+        assert.deepEqual(day05.run_program(memory, 9), [1001, 3]);
+    });
+    it('part2', function () {
+        var memory = utils.get_input('day05', ',', Number);
+        // TODO: why is there a trailing 314?
+        assert.deepEqual(day05.part2(memory), [3188550, 314]);
     });
 });
