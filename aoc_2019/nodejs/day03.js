0b576e88c241156525db2b9f830b9b0897767694
Dan Stine <sw@stinemail.com>
2019-12-08 14:54:32 +0000

    avoid var keyword

--- aoc_2019/nodejs/day03.js
+++ aoc_2019/nodejs/day03.js
@@ -15,40 +15,40 @@ module.exports = {
     part2,
 };
 
-var ORIGIN = new Point(0, 0, 0);
+const ORIGIN = new Point(0, 0, 0);
 
 function walk(moves) {
-    var points = [];
-    var segment_start = ORIGIN;
-    var segment = 0;
-    var total_steps = 0;
+    const points = [];
+    let segment_start = ORIGIN;
+    let segment = 0;
+    let total_steps = 0;
     while (segment < moves.length) {
-        var x = segment_start.x;
-        var y = segment_start.y;
-        var move = moves[segment];
-        var dir = move.substring(0,1);
-        var steps = Number(move.substring(1));
+        const x = segment_start.x;
+        const y = segment_start.y;
+        const move = moves[segment];
+        const dir = move.substring(0,1);
+        const steps = Number(move.substring(1));
         switch (dir) {
             case "U":
-                for (var s=1; s<=steps; s++) {
+                for (let s=1; s<=steps; s++) {
                     total_steps += 1;
                     points.push(new Point(x, y+s, total_steps));
                 }
                 break;
             case "D":
-                for (var s=1; s<=steps; s++) {
+                for (let s=1; s<=steps; s++) {
                     total_steps += 1;
                     points.push(new Point(x, y-s, total_steps));
                 }
                 break;
             case "R":
-                for (var s=1; s<=steps; s++) {
+                for (let s=1; s<=steps; s++) {
                     total_steps += 1;
                     points.push(new Point(x+s, y, total_steps));
                 }
                 break;
             case "L":
-                for (var s=1; s<=steps; s++) {
+                for (let s=1; s<=steps; s++) {
                     total_steps += 1;
                     points.push(new Point(x-s, y, total_steps));
                 }
@@ -65,13 +65,13 @@ function dist(a, b) {
 }
 
 function intersection(w1_moves, w2_moves) {
-    var w1_points = walk(w1_moves);
-    var w2_points = walk(w2_moves);
-    var intersection = [];
-    for (var i=0; i<w1_points.length; i++) {
-        var w1_point = w1_points[i];
-        for (var j=0; j<w2_points.length; j++) {
-            var w2_point = w2_points[j];
+    const w1_points = walk(w1_moves);
+    const w2_points = walk(w2_moves);
+    let intersection = [];
+    for (let i=0; i<w1_points.length; i++) {
+        const w1_point = w1_points[i];
+        for (let j=0; j<w2_points.length; j++) {
+            const w2_point = w2_points[j];
             if (w1_point.x == w2_point.x && w1_point.y == w2_point.y) {
                 intersection.push([w1_point, w2_point]);
                 break;
@@ -82,22 +82,22 @@ function intersection(w1_moves, w2_moves) {
 }
 
 function part1(w1_moves, w2_moves) {
-    var int = intersection(w1_moves, w2_moves);
-    var min = Number.POSITIVE_INFINITY;
+    const int = intersection(w1_moves, w2_moves);
+    let min = Number.POSITIVE_INFINITY;
     int.forEach(function(points) {
         // either point will do since we don't need total_steps
-        var point = points[0];
-        var d = dist(ORIGIN, point);
+        const point = points[0];
+        const d = dist(ORIGIN, point);
         min = Math.min(d, min);
     });
     return min;
 }
 
 function part2(w1_moves, w2_moves) {
-    var int = intersection(w1_moves, w2_moves);
-    var min = Number.POSITIVE_INFINITY;
+    const int = intersection(w1_moves, w2_moves);
+    let min = Number.POSITIVE_INFINITY;
     int.forEach(function(points) {
-        var total_steps = points[0].total_steps + points[1].total_steps;
+        const total_steps = points[0].total_steps + points[1].total_steps;
         min = Math.min(total_steps, min);
     });
     return min;
--- aoc_2019/nodejs/test/day03_test.js
+++ aoc_2019/nodejs/test/day03_test.js
@@ -1,13 +1,13 @@
-var assert = require('assert');
+const assert = require('assert');
 
-var utils = require('./test_utils.js');
-var day03 = require('../day03.js');
+const utils = require('./test_utils.js');
+const day03 = require('../day03.js');
 
 describe('day03', function() {
     it('walk', function () {
-        var w1_moves = ['R8','U5','L5','D3'];
-        var w1_actual = day03.walk(w1_moves);
-        var w1_expected = [
+        const w1_moves = ['R8','U5','L5','D3'];
+        const w1_actual = day03.walk(w1_moves);
+        const w1_expected = [
                 [1,0,1], [2,0,2], [3,0,3], [4,0,4], [5,0,5], [6,0,6], [7,0,7], [8,0,8],
                 [8,1,9], [8,2,10], [8,3,11], [8,4,12], [8,5,13],
                 [7,5,14], [6,5,15], [5,5,16], [4,5,17], [3,5,18],
@@ -21,18 +21,18 @@ describe('day03', function() {
         assert.equal(day03.dist(new day03.Point(1,2), new day03.Point(-6,1)), 8);
     });
     it('intersection', function () {
-        var w1_moves = ['R8','U5','L5','D3'];
-        var w2_moves = ['U7','R6','D4','L4'];
-        var actual = day03.intersection(w1_moves, w2_moves);
-        var expected = [
+        const w1_moves = ['R8','U5','L5','D3'];
+        const w2_moves = ['U7','R6','D4','L4'];
+        const actual = day03.intersection(w1_moves, w2_moves);
+        const expected = [
             [new day03.Point(6,5,15), new day03.Point(6,5,15)],
             [new day03.Point(3,3,20), new day03.Point(3,3,20)]
         ];
         assert.deepEqual(actual, expected);
     });
     it('part1-example', function () {
-        var w1_moves = ['R8','U5','L5','D3'];
-        var w2_moves = ['U7','R6','D4','L4'];
+        let w1_moves = ['R8','U5','L5','D3'];
+        let w2_moves = ['U7','R6','D4','L4'];
         assert.equal(day03.part1(w1_moves, w2_moves), 6);
         w1_moves = 'R75,D30,R83,U83,L12,D49,R71,U7,L72'.split(',');
         w2_moves = 'U62,R66,U55,R34,D71,R55,D58,R83'.split(',');
@@ -43,14 +43,14 @@ describe('day03', function() {
     });
     it('part1', function () {
         this.timeout(200000);
-        var moves = utils.get_input('day03');
-        var w1_moves = moves[0].split(',');
-        var w2_moves = moves[1].split(',');
+        const moves = utils.get_input('day03');
+        const w1_moves = moves[0].split(',');
+        const w2_moves = moves[1].split(',');
         assert.equal(day03.part1(w1_moves, w2_moves), 1431);
     });
     it('part2-example', function () {
-        var w1_moves = ['R8','U5','L5','D3'];
-        var w2_moves = ['U7','R6','D4','L4'];
+        let w1_moves = ['R8','U5','L5','D3'];
+        let w2_moves = ['U7','R6','D4','L4'];
         assert.equal(day03.part2(w1_moves, w2_moves), 30);
         w1_moves = 'R75,D30,R83,U83,L12,D49,R71,U7,L72'.split(',');
         w2_moves = 'U62,R66,U55,R34,D71,R55,D58,R83'.split(',');
@@ -61,9 +61,9 @@ describe('day03', function() {
     });
     it('part2', function () {
         this.timeout(200000);
-        var moves = utils.get_input('day03');
-        var w1_moves = moves[0].split(',');
-        var w2_moves = moves[1].split(',');
+        const moves = utils.get_input('day03');
+        const w1_moves = moves[0].split(',');
+        const w2_moves = moves[1].split(',');
         assert.equal(day03.part2(w1_moves, w2_moves), 48012);
     });
 });
