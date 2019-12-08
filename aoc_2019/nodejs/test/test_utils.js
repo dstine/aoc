170403bb73d8a12c30db011c9fe71e049254165c
Dan Stine <sw@stinemail.com>
2019-12-08 14:58:02 +0000

    avoid var keyword

--- aoc_2019/nodejs/test/test_utils.js
+++ aoc_2019/nodejs/test/test_utils.js
@@ -3,21 +3,21 @@ module.exports = {
     get_example,
 };
 
-var fs = require('fs');
-var path = require('path');
+const fs = require('fs');
+const path = require('path');
 
 function get_input(day, separator='\n', map_fn=Object) {
-    var input_path = path.join(__dirname, `../../data/${day}_input.txt`);
+    const input_path = path.join(__dirname, `../../data/${day}_input.txt`);
     return get_contents(input_path, separator, map_fn);
 }
 
 function get_example(day, separator='\n', map_fn=Object, suffix='') {
-    var input_path = path.join(__dirname, `../../data/${day}_example${suffix}.txt`);
+    const input_path = path.join(__dirname, `../../data/${day}_example${suffix}.txt`);
     return get_contents(input_path, separator, map_fn);
 }
 
 function get_contents(input_path, separator='\n', map_fn=Object) {
-    var input = fs.readFileSync(input_path, 'utf-8');
-    var vals = input.split(separator).map(map_fn);
+    const input = fs.readFileSync(input_path, 'utf-8');
+    const vals = input.split(separator).map(map_fn);
     return vals;
 }
